49e2ab27820bb764da85057407af87d1449fb237
AZCoder018 <jason.meadows.esq@hotmail.com>
2018-07-09 03:02:21 -0700

    changed connection for JAWSDB

--- app/config/connection.js
+++ app/config/connection.js
@@ -5,7 +5,17 @@
 // Dependencies
 var Sequelize = require("sequelize");
 
-// Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
+var sequelize;
+
+//JAWSDB Connection for Heroku; if not, MySQL database
+if (process.env.JAWSDB_URL)
+{
+	sequelize = Sequelize.createConnection(process.env.JAWSDB_URL);
+}
+else
+{
+
+// Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.//
 var sequelize = new Sequelize("juniorpicasso", "root", "DrPepper", {
   host: "localhost",
   dialect: "mysql",
@@ -15,6 +25,7 @@ var sequelize = new Sequelize("juniorpicasso", "root", "DrPepper", {
     idle: 10000
   }
 });
+}
 
 // Exports the connection for other files to use
 module.exports = sequelize;
